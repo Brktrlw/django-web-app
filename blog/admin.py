8e6cda0096761afd11c69bf7c3d94f04c522021a
Berkay Şen <brktrl@protonmail.ch>
2021-12-01 12:09:20 +0300

    Update

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import BlogModel,KategoriModel
+from .models import BlogModel,KategoriModel,CommentModel
 
 admin.site.register(KategoriModel)
 admin.site.register(BlogModel)
+
+
+class YorumAdmin(admin.ModelAdmin):
+    list_display = ('commentAuthor','blogId','createdTime')
+    search_fields = ('commentAuthor__username',)
+
+admin.site.register(CommentModel,YorumAdmin)
