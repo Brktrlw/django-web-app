45e5581e88b6790d5f72770c3e6b58ec0820db4e
Berkay Şen <brktrl@protonmail.ch>
2021-12-13 00:21:23 +0300

    Update

--- config/POSTS/models.py
+++ config/POSTS/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class PostModel(models.Model):
+    postAuthor      = models.ForeignKey("auth.User",on_delete=models.CASCADE,verbose_name="Yazar")
+    postTitle       = models.CharField(max_length=100,verbose_name="Post Başlığı")
+    postDescription = models.TextField(max_length=500,)
+    postCreatedDate = models.DateTimeField(auto_now_add=True)
+    postEditedDate  = models.DateTimeField(auto_now=True)
+
+
