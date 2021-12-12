4945f2ad75ec77cddf7b0d949abcac6b7f5a4c82
Berkay Şen <brktrl@protonmail.ch>
2021-12-12 20:47:02 +0300

    Update

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import vPosts,vPostCreate,vPostDetail
-
-urlpatterns = [
-    path('',vPosts,name="posts"),
-    path('create/',vPostCreate),
-    path('<str:PostName>/',vPostDetail)
-]
--- django_blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def about(request):
-    return render(request,"about.html")
-
-def mainIndex(request):
-    return render(request,"mainIndex.html")
