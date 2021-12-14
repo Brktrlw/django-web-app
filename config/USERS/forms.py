e1411697cb8dfdc9f5bb248e2a13aa0f741ed541
Berkay Şen <brktrl@protonmail.ch>
2021-12-14 15:47:27 +0300

    Update

--- config/USERS/forms.py
+++ config/USERS/forms.py
@@ -3,7 +3,16 @@ from django import forms
 class ContactForm(forms.Form):
     Name    = forms.CharField(max_length=50,label="Ad",required=True,widget=forms.TextInput(attrs={"placeholder":"Adınız"}))
     Email   = forms.EmailField(max_length=200,label="Email",required=True,widget=forms.EmailInput(attrs={"placeholder":"E mail adresiniz"}))
-    Message = forms.CharField(max_length=1000,label="Mesajınız",required=True,widget=forms.Textarea(attrs={"placeholder":"Mesajınız"}))
+    Message = forms.CharField(max_length=1000,label="Mesajınız",required=True)
+
+
+
+    def __init__(self,*args,**kwargs):
+        super(ContactForm,self).__init__(*args,**kwargs)
+        self.fields['Message'].widget=forms.Textarea(attrs={"placeholder":"Mesajınızı Giriniz","class":"form-control"})
+        self.fields['Name'].widget=forms.TextInput(attrs={"placeholder":"Adınızı Giriniz","class":"form-control"})
+        self.fields['Email'].widget=forms.TextInput(attrs={"placeholder":"Mail adresinizi Giriniz","class":"form-control"})
+
 
 
     def clean_Name(self):
@@ -11,5 +20,6 @@ class ContactForm(forms.Form):
         if Name=="a":
             raise forms.ValidationError("Lütfen başka harf giriniz")
         return Name
+
 
 
